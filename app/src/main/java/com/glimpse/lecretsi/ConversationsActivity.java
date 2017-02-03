3bef7834615bfef0f5aae1f1bc449baaf675c34e
Adrian Popa <adipopa9910@gmail.com>
2017-02-03 19:00:32 +0200

    Linked MainActivity to ConversationsActivity

--- app/src/main/java/com/glimpse/lecretsi/ConversationsActivity.java
+++ app/src/main/java/com/glimpse/lecretsi/ConversationsActivity.java
@@ -1,5 +1,6 @@
 package com.glimpse.lecretsi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +13,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -53,6 +56,18 @@ public class ConversationsActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
         mainList = (ListView)findViewById(R.id.conversationsList);
+
+        mainList.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Intent intent = new Intent(ConversationsActivity.this, MainActivity.class);
+                        /* String conversationID = conversationIDText.getText().toString();
+                        intent.putExtra("conversationID", conversationID); */
+                        startActivity(intent);
+                    }
+                }
+        );
 
         ArrayList<HashMap<String, String>> conversationsList = new ArrayList<>();
 
