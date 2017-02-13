1189e0ff5067a65ebd260968954993710528cc16
andreibratu <bratu.andrei@outlook.com>
2017-02-13 13:54:23 +0200

    AboutActivity

--- app/src/main/java/com/glimpse/lecretsi/AboutActivity.java
+++ app/src/main/java/com/glimpse/lecretsi/AboutActivity.java
@@ -13,10 +13,24 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
 
         TextView developerTitle = (TextView)findViewById(R.id.developers_title);
-        developerTitle.setText("Les auteurs");
+        developerTitle.setText(R.string.about_dev_title);
         developerTitle.setTextSize(30);
         developerTitle.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
 
+        TextView developerText = (TextView)findViewById(R.id.developers_text);
+        developerText.setText(R.string.about_dev_text);
+        developerText.setTextSize(20);
+        developerText.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+
+        TextView testerTitle = (TextView)findViewById(R.id.testers_title);
+        testerTitle.setText(R.string.about_testers_title);
+        testerTitle.setTextSize(30);
+        testerTitle.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+
+        TextView testerText = (TextView)findViewById(R.id.testers_text);
+        testerText.setText(R.string.about_testers_text);
+        testerText.setTextSize(20);
+        testerText.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
 
     }
 }
