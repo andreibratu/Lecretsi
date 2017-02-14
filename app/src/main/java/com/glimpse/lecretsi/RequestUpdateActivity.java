6d49e464de4ef01be7194cffb31e8d09005c7217
Adrian Popa <adipopa9910@gmail.com>
2017-02-14 15:53:21 +0200

    Added RequestUpdateActivity

---
+++ app/src/main/java/com/glimpse/lecretsi/RequestUpdateActivity.java
@@ -0,0 +1,26 @@
+package com.glimpse.lecretsi;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RequestUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_update);
+
+        TextView updateLink = (TextView) findViewById(R.id.updateLink);
+        updateLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.update_link)));
+                startActivity(browserIntent);
+            }
+        });
+    }
+}
